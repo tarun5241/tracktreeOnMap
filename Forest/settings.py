0c750114bcb6b9a1320489ddf301180cc1c4164f
tarun5241 <tarunsharmapatwa@gmail.com>
2022-11-20 20:22:24 +0530

    changes in setting

--- Forest/settings.py
+++ Forest/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l-!*ovrej0-=%6=1kvi^k^bh$62rcpb_0+87#ia5*afq=2ge@b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
